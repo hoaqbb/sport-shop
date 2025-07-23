3a2dbbf924377c5432ae3cdbfd46f8154566c160
= <dinhhoang210903@gmail.com>
2025-07-23 23:03:05 +0700

    feat: Add application layer for product management with commands and queries

---
+++ Services/Catalog/Catalog.Application/Handlers/GetAllTypesHandler.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Handlers
+{
+    internal class GetAllTypesHandler : IRequestHandler<GetAllTypesQuery, IList<TypeResponse>>
+    {
+        private readonly ITypeRepository _typeRepository;
+
+        public GetAllTypesHandler(ITypeRepository typeRepository)
+        {
+            _typeRepository = typeRepository;
+        }
+        public async Task<IList<TypeResponse>> Handle(GetAllTypesQuery request, CancellationToken cancellationToken)
+        {
+            var typeList = await _typeRepository.GetAllTypes();
+            var typeResponseList = ProductMapper.Mapper.Map<IList<TypeResponse>>(typeList);
+
+            return typeResponseList;
+        }
+    }
+}
---
+++ Services/Catalog/Catalog.Application/Mappers/ProductMapper.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+
+namespace Catalog.Application.Mappers
+{
+    public static class ProductMapper
+    {
+        private static readonly Lazy<IMapper> Lazy = new Lazy<IMapper>(() =>
+        {
+            MapperConfiguration config = new MapperConfiguration(cfg =>
+            {
+                cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+                cfg.AddProfile<ProductMappingProfile>();
+            });
+            
+            var mapper = config.CreateMapper();
+
+            return mapper;
+        });
+
+        public static IMapper Mapper => Lazy.Value;
+    }
+}
