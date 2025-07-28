664fabd1aa1376b51687c0f39d16c5d6afd440cb
= <dinhhoang210903@gmail.com>
2025-07-28 14:04:04 +0700

    feat: Implement Ordering service with Entity Framework Core and repository pattern

---
+++ Services/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if(!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation($"Ordering Database: {typeof(OrderContext).Name} seeded!!!");
+            }
+        }
+
+        private static IEnumerable<Order> GetOrders()
+        {
+            return new List<Order>
+            {
+                new ()
+                {
+                    UserName = "hoang",
+                    FirstName = "Dinh",
+                    LastName = "Hoang",
+                    EmailAddress = "hoang@sport-shop.net",
+                    AddressLine = "Ho Chi Minh",
+                    Country = "VietNam",
+                    TotalPrice = 750,
+                    State = "HCM",
+                    ZipCode = "510000",
+
+                    CardName = "Visa",
+                    CardNumber = "1234567890123456",
+                    CreatedBy = "Hoang",
+                    Expiration = "12/25",
+                    Cvv = "123",
+                    PaymentMethod = 1,
+                    LastModifiedBy = "Hoang",
+                    LastModifiedDate = new DateTime(),
+                }
+            };
+        }
+    }
+}
---
+++ Services/Ordering/Ordering.Infrastructure/Extensions/InfrastructureServices.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Core.Repositories;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Repositories;
+
+namespace Ordering.Infrastructure.Extensions
+{
+    public static class InfrastructureServices
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<OrderContext>(options => options.UseSqlServer(config.GetConnectionString("OrderingConnectionString")));
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            return services;
+        }
+    }
+}
