7fd05fca5f22d14698894338d7afb2644c2cf669
= <dinhhoang210903@gmail.com>
2025-07-29 21:31:44 +0700

    feat: Add Ordering service with command handlers, validators, and exception handling

---
+++ Services/Ordering/Ordering.Application/Validators/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using Ordering.Application.Commands;
+
+namespace Ordering.Application.Validators
+{
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(o => o.UserName)
+                .NotEmpty()
+                .WithMessage("{Username} is required.")
+                .NotNull()
+                .MaximumLength(70)
+                .WithMessage("{Username} must not exceed 70 characters");
+            RuleFor(o => o.TotalPrice)
+                .NotEmpty()
+                .WithMessage("{TotalPrice} is required.")
+                .GreaterThan(-1)
+                .WithMessage("{TotalPrice} must be greater than or equal to 0.");
+            RuleFor(o => o.EmailAddress)
+                .NotEmpty()
+                .WithMessage("{EmailAddress} is required.");
+            RuleFor(o => o.FirstName)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("{Firstname} is required.");
+            RuleFor(o => o.LastName)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("{LastName} is required.");
+        }
+    }
+}
---
+++ Services/Ordering/Ordering.Application/Validators/UpdateOrderCommandValidator.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using Ordering.Application.Commands;
+
+namespace Ordering.Application.Validators
+{
+    public class UpdateOrderCommandValidator : AbstractValidator<UpdateOrderCommand>
+    {
+        public UpdateOrderCommandValidator()
+        {
+            RuleFor(o => o.Id)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("{Id} is required.")
+                .GreaterThan(0)
+                .WithMessage("{Id} must be greater than 0.");
+            RuleFor(o => o.UserName)
+                .NotEmpty()
+                .WithMessage("{Username} is required.")
+                .NotNull()
+                .MaximumLength(70)
+                .WithMessage("{Username} must not exceed 70 characters");
+            RuleFor(o => o.TotalPrice)
+                .NotEmpty()
+                .WithMessage("{TotalPrice} is required.")
+                .GreaterThan(-1)
+                .WithMessage("{TotalPrice} must be greater than or equal to 0.");
+            RuleFor(o => o.EmailAddress)
+                .NotEmpty()
+                .WithMessage("{EmailAddress} is required.");
+            RuleFor(o => o.FirstName)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("{Firstname} is required.");
+            RuleFor(o => o.LastName)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("{LastName} is required.");
+        }
+    }
+}
