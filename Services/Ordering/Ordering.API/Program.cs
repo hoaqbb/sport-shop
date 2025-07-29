06bdd3eaee72738e136bb8f5ff6c000ee828efc6
= <dinhhoang210903@gmail.com>
2025-07-29 21:41:29 +0700

    feat: Implement Ordering service with API controllers, database migrations, and Docker support

---
+++ Services/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,51 @@
+using Asp.Versioning;
+using Ordering.API.Extensions;
+using Ordering.Application.Extensions;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+//Add API Versioning
+builder.Services.AddApiVersioning(options =>
+{
+    options.ReportApiVersions = true;
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Ordering.API", Version = "v1" }); });
+
+//Add Application Services
+builder.Services.AddApplicationServices();
+
+//Add Infrastructure Services
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+var app = builder.Build();
+
+//Apply database migrations
+app.MigrateDatabase<OrderContext>((context, services) =>
+{
+    var logger = services.GetService<ILogger<OrderContextSeed>>();
+    OrderContextSeed.SeedAsync(context, logger).Wait();
+});
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
