228ec44b5ef67f3e5aa0b379fb84608aa8962df6
= <dinhhoang210903@gmail.com>
2025-08-11 21:28:03 +0700

    feat: Add error handling interceptor

--- Client/src/app/core/core.module.ts
+++ Client/src/app/core/core.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
-
-
+import { NotFoundComponent } from './not-found/not-found.component';
+import { UnAuthenticatedComponent } from './un-authenticated/un-authenticated.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
 
 @NgModule({
-  declarations: [NavBarComponent],
-  imports: [
-    CommonModule,
-    RouterModule
+  declarations: [
+    NavBarComponent,
+    NotFoundComponent,
+    UnAuthenticatedComponent,
+    ServerErrorComponent,
   ],
-  exports: [NavBarComponent]
+  imports: [CommonModule, RouterModule],
+  exports: [NavBarComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
